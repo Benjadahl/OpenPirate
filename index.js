718da921c1a83229e409987dbc0efbefb66b84aa
benjadahl <benjadahl@gmail.com>
2016-09-10 20:36:14 +0200

    Added the changevelocity function

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  socket.on('velocityChange', function(faster){
+    if(faster){
+      var vel = players[socket.id].velocity;
+      if(vel <= 10){
+        vel += 1;
+      }
+    }else{
+      if(vel >= -5){
+        vel -= 1;
+      }
+    }
+  });
   addPlayer(socket.id, 100, 0, 0, 0, 0);
   console.log(players);
 });
