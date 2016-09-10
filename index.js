2b1ff792d07c8acb53882fff47e5332ec08e275a
benjadahl <benjadahl@gmail.com>
2016-09-10 20:20:45 +0200

    Added player variables

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var players = {};
+
+function addPlayer(id, hp, x, y, velocity, orientation){
+  players[id] = {"hp": hp, "x": x, "y": y, "velocity": velocity, "orientation": orientation};
+}
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  addPlayer(socket.id, 100, 0, 0, 0, 0);
+  console.log(players);
 });
 
 http.listen(3000, function(){
