40f0ed933d23e2274dcf29a3eb539a91fe1ffe01
benjadahl <benjadahl@gmail.com>
2016-09-10 21:09:42 +0200

    Fixed velocity

--- index.js
+++ index.js
@@ -17,29 +17,37 @@ app.get('/ship.png', function(req, res){
 
 var players = {};
 
-function addPlayer(id, hp, x, y, velocity, orientation){
-  players[id] = {"hp": hp, "x": x, "y": y, "velocity": velocity, "orientation": orientation};
+function addPlayer(id, hp, x, y, velocity, velocityChange, orientation){
+  players[id] = {"hp": hp, "x": x, "y": y, "velocity": velocity, "velocityChange": velocityChange, "orientation": orientation};
+}
+
+function updateVelocity(){
+  for (var p in players){
+    console.log(players);
+    var vel = players[p].velocity;
+    if (vel <= 10 && vel >= -5){
+      players[p].velocity += players[p].velocityChange;
+    }
+  }
 }
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
-  socket.on('velocityChange', function(faster){
-    if(faster){
-      var vel = players[socket.id].velocity;
-      if(vel <= 10){
-        vel += 1;
-      }
-    }else{
-      if(vel >= -5){
-        vel -= 1;
-      }
+  socket.on('velocityChange', function(change){
+    if (change === -1 || change === 0 || change === 1){
+      players[socket.id].velocityChange = change;
     }
   });
-  addPlayer(socket.id, 100, 0, 0, 0, 0);
+  socket.on
+  addPlayer(socket.id, 100, 0, 0, 0, 0, 0);
   console.log(players);
 });
+
+setInterval(function() {
+  updateVelocity();
+}, 50);
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
